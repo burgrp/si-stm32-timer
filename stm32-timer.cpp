7f10a08359a5c90035e9fa9edae34eb30f2d161e
Pavel Burgr <pb@drake.cz>
2018-07-31 02:44:54 +0200

    WIP

--- stm32-timer.cpp
+++ stm32-timer.cpp
@@ -1,7 +1,7 @@
 namespace genericTimer {
 
-	void initHardware() {
-		target::STK.RVR.setRELOAD(8E6 / 100L);
+	void initHardware(int clkHz) {
+		target::STK.RVR.setRELOAD(clkHz / 100L);
 		target::STK.CVR.setCURRENT(0);
 		target::STK.CSR.setTICKINT(1);
 		target::STK.CSR.setCLKSOURCE(1);
