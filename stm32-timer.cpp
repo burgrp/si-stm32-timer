52fe30c723d275c6395e711894f005d0424be627
Pavel Burgr <pb@drake.cz>
2022-09-09 15:57:50 +0200

    WIP

--- stm32-timer.cpp
+++ stm32-timer.cpp
@@ -5,9 +5,16 @@ namespace genericTimer {
 	void initHardware() {
 		target::SYSTICK.RVR.setRELOAD(clkHz / 100L);
 		target::SYSTICK.CVR.setCURRENT(0);
-		target::SYSTICK.CSR.setTICKINT(target::systick::CSR::TICKINT::ENABLE_SYSTICK_EXCEPTION);
+
+		// this is error in SVD - DISABLE_SYSTICK_EXCEPTION and ENABLE_SYSTICK_EXCEPTION are swapped
+		// hence we use integer value here
+		// target::systick::CSR::TICKINT::DISABLE_SYSTICK_EXCEPTION
+		// target::systick::CSR::TICKINT::ENABLE_SYSTICK_EXCEPTION
+		target::SYSTICK.CSR.setTICKINT((target::systick::CSR::TICKINT)1);
+		
 		target::SYSTICK.CSR.setCLKSOURCE(target::systick::CSR::CLKSOURCE::CPU_CLOCK);
 		target::SYSTICK.CSR.setENABLE(target::systick::CSR::ENABLE::ENABLED);
+		
 	}
 
 }
